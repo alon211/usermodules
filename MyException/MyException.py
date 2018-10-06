7e591fa69b9b06337a20241f4ec6f55fb8682366
alon211 <306068447@qq.com>
2018-10-06 16:55:27 +0800

    add exception module

--- MyException/MyException.py
+++ MyException/MyException.py
@@ -1,3 +1,7 @@
+FILE_ERROR_INFOR={
+    'FILE_NOT_FOUNT':"file can not founded,please check your path",
+    'FILE_EXTESION_ERROR':'文件扩展名不匹配'
+}
 class MyException(Exception):
     def __int__(self,*args):
         self.args=args
